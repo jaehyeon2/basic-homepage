1f144b5d43782cf6da9ad5530c301973d27dcf8e
jaehyeon2 <wol5971@gmail.com>
2022-02-18 16:58:21 +0900

    model index.js

---
+++ models/index.js
@@ -0,0 +1,31 @@
+const Sequelize=require('sequelize');
+const env=process.NODE_ENV||'development';
+const config=require("../config/config")[env];
+
+const User=require("./user");
+const Page=require("./page");
+const Board=require("./board");
+const Post=require("./post");
+
+const db={};
+const sequelize=new Sequelize(
+    config.database, config.username, config.password, config,
+    );
+
+db.sequelize=sequelize;
+db.User=User;
+db.Page=Page;
+db.Board=Board;
+db.Post=Post;
+
+User.init(sequelize);
+Page.init(sequelize);
+Board.init(sequelize);
+Post.init(sequelize);
+
+User.associate(db)
+Page.associate(db);
+Board.associate(db);
+Post.associate(db);
+
+module.exports=db;
